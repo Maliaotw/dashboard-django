603125d6e6824d70b02cbfcd1a4c0d06084664ee
Maliao Guo <MaliaoTW@gmail.com>
2021-01-03 02:16:26 +0800

    make it better

--- web/config/settings/heroku.py
+++ web/config/settings/heroku.py
@@ -4,5 +4,4 @@ import dj_database_url
 MIDDLEWARE.append('django.middleware.security.SecurityMiddleware')
 MIDDLEWARE.append('whitenoise.middleware.WhiteNoiseMiddleware')
 
-DATABASES = {}
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+
