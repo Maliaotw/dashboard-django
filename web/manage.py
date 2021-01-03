88b6f887a6c75844a2e898fffd4424240cf2ad01
Maliao Guo <MaliaoTW@gmail.com>
2021-01-03 12:27:03 +0800

    make it better

--- web/manage.py
+++ web/manage.py
@@ -5,7 +5,9 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
+    if not os.getenv("DJANGO_SETTINGS_MODULE"):
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
