40dc76efb484d781094f2fd118779bffeda89973
Maliao Guo <MaliaoTW@gmail.com>
2021-01-03 12:08:12 +0800

    make it better

--- web/config/settings/heroku.py
+++ web/config/settings/heroku.py
@@ -4,4 +4,7 @@ import dj_database_url
 MIDDLEWARE.append('django.middleware.security.SecurityMiddleware')
 MIDDLEWARE.append('whitenoise.middleware.WhiteNoiseMiddleware')
 
-
+# load database from the DATABASE_URL environment variable
+DATABASE_URL = CONFIG.HEROKU_POSTGRESQL_GOLD_URL
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
